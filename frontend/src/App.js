6c66e6aa7a61e6ad13121a003169530eb91d0ee6
FredrikH03 <fredrikholst03@hotmail.com>
2024-08-20 13:44:31 +0200

    split up the folders so we have a frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Navbar from './Components/Navbar.js'
+import Footer from './Components/Footer.js'
+import ArticleCard from './Components/ArticleCards.js'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <ArticleCard />
+      <Footer />
+
+    </div>
+  );
+}
+
+
+
+export default App;
